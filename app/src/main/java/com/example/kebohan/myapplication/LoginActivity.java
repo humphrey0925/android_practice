3610dc602bfcc6ac12e7945c4fc28c1f0b0f0762
humphrey0925 <kebohan1@gmail.com>
2017-10-31 11:54:09 +0800

    20171031一改 增加登錄功能與服務器對接

--- app/src/main/java/com/example/kebohan/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/kebohan/myapplication/LoginActivity.java
@@ -2,25 +2,22 @@ package com.example.kebohan.myapplication;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
-import java.net.URLConnection;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 
 import org.json.JSONArray;
@@ -29,18 +26,17 @@ import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
     public ProgressDialog PDialog = null;
-    public EditText username;
+    public EditText username,userpassword;
+    public JSONArray userinfo;
+    private int userlogin_status;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
         username = (EditText) findViewById(R.id.username);
+        userpassword = (EditText) findViewById(R.id.userpassword);
         Button sign_in = (Button) findViewById(R.id.sign_in_button);
-
-        String TAG_STRING ="file path";
-
-
         Button login = (Button) findViewById(R.id.login_button);
         sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,20 +59,8 @@ public class LoginActivity extends AppCompatActivity {
                     new Thread(){
                         public void run(){
                             try{
-                                try {
-                                    String login_url= "192.168.0.164/login_url.php?view="+username.getText().toString();
-                                    URL url = new URL(login_url);
-                                    // 得到HttpURLConnection对象
-                                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                                    // 设置为GET方式
-                                    connection.setRequestMethod("GET");
-                                    if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                                        // 得到响应消息
-                                        String message = connection.getResponseMessage();
-                                    }
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
+                                new TransTask().execute("192.168.43.253/login.php?username="+username.getText().toString()+"&password="+userpassword.getText().toString());
+
                             }
                             catch(Exception e){
                                 e.printStackTrace();
@@ -100,19 +84,121 @@ public class LoginActivity extends AppCompatActivity {
                             })
                             .show();
                 }
+                switch (userlogin_status)
+                {
+                    case 200:
+                        MainAct();
+                        break;
+                    case 403:
+
+                        break;
+                    case 404:
+                        break;
+
+                }
 
             }
         });
 
     }
+    class TransTask extends AsyncTask<String, Void,String>
+    {
 
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder sb = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(url.openStream()));
+                String line = in.readLine();
+                while(line!=null){
+                    Log.d("HTTP", line);
+                    sb.append(line);
+                    line = in.readLine();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return sb.toString();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("JSON", s);
+            user_login_auth(s);
+        }
+
+
+    }
+    private void user_login_auth(String s){
+        try {
+            userinfo = new JSONArray(s);
+            JSONObject userinfo_item = userinfo.getJSONObject(0);
+            if (userinfo_item!=null)
+            {
+                if(userinfo_item.getString("status")!="403")
+                {
+                    userlogin_status=200;
+                }
+                else
+                {
+                    userlogin_status=403;
+                }
+            }
+            else
+            {
+                userlogin_status=404;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public void pop_out_error()
+    {
+        String status_title = null,status_message=null;
+        switch (userlogin_status)
+        {
+            case 403:
+                status_title=getString(R.string.login_error_403_title);
+                status_message=getString(R.string.login_error_403_text);
+                break;
+            case 404:
+                status_title=getString(R.string.login_error_404_title);
+                status_message=getString(R.string.login_error_404_text);
+                break;
+
+        }
+        new AlertDialog.Builder(LoginActivity.this)
+
+
+                .setTitle(status_title)
+                .setMessage(status_message)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .show();
+    }
     public void sign_up_page()
     {
         Intent sign_up = new Intent(LoginActivity.this,sign.class);
         startActivity(sign_up);
     }
+    public void MainAct()
+    {
+        Intent main = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(main);
+    }
     public int login_auth()
     {
+
         EditText username = (EditText) findViewById(R.id.username);
         EditText userpassword = (EditText) findViewById(R.id.userpassword);
         String username_str = username.getText().toString();
